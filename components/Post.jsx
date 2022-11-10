628cfdbf5ec8b05111549855d88e1de169d1a5a0
Ainis Taranda <taranda.ainis@gmail.com>
2022-11-10 10:51:39 -0500

    feed working

---
+++ components/Post.jsx
@@ -0,0 +1,20 @@
+import { Avatar } from "@rneui/themed";
+import { View, Text, ActivityIndicator, Image } from "react-native";
+import styles from "../styles";
+
+export default function Post({ post }) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardHeader}>
+        <Avatar source={{ uri: post.profilePhoto }} rounded />
+        <Text style={styles.userName}>{post.userName}</Text>
+      </View>
+      <Image
+        style={styles.image}
+        source={{ uri: post.photoURL }}
+        PlaceholderContent={<ActivityIndicator />}
+      />
+      <Text style={styles.description}>{post.description}</Text>
+    </View>
+  );
+}
---
+++ photolist.js
@@ -0,0 +1,42 @@
+//put a fake list of photos instead of hitting an api
+
+const photolist = [
+    {id: 1, 
+    photoURL: 'https://images.unsplash.com/photo-1665680521183-1a77175d81dd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTMxMQ&ixlib=rb-1.2.1&q=80&w=1080',
+    description: 'overhead image of pine trees',
+    userName: 'Tree Hugger',
+    profilePhoto: 'https://randomuser.me/api/portraits/women/3.jpg'
+    },
+    {id: 2,
+    photoURL: 'https://images.unsplash.com/photo-1667344792187-6b3428082c8f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTQ3Mg&ixlib=rb-4.0.3&q=80&w=1080',
+    description: 'outdoor coffee shop with people ordering and walking on sidewalk',
+    userName: 'Caffeine Fiend',
+    profilePhoto: 'https://randomuser.me/api/portraits/men/3.jpg'
+    },
+    {id: 3, 
+    photoURL: 'https://images.unsplash.com/photo-1666834531292-b2a746be5e7b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTU4MQ&ixlib=rb-4.0.3&q=80&w=1080',
+    description: 'Calm ocean beach scene with small waves breaking on shore',
+    userName: 'H20 = life',
+    profilePhoto: 'https://randomuser.me/api/portraits/women/10.jpg'
+    },
+    {id: 4, 
+    photoURL: 'https://images.unsplash.com/photo-1667802694056-3dd951aba710?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTY0Nw&ixlib=rb-4.0.3&q=80&w=1080',
+    description: 'Overhead scene of Christmas gift on table with candle and decorations',
+    userName: 'bob',
+    profilePhoto: 'https://randomuser.me/api/portraits/men/10.jpg'
+    },
+    {id: 5, 
+    photoURL: 'https://images.unsplash.com/photo-1666350652958-4125cbfe9163?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTc3Mg&ixlib=rb-4.0.3&q=80&w=1080',
+    description: 'Person with a pumkin carving on head leaning on fence',
+    userName: 'Pumkin Head',
+    profilePhoto: 'https://randomuser.me/api/portraits/women/20.jpg'
+    },
+    {id: 6, 
+    photoURL: 'https://images.unsplash.com/photo-1659776026027-6b0f66d92675?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTgzMg&ixlib=rb-4.0.3&q=80&w=1080',
+    description: 'Metalic oil slick rendering',
+    userName: 'Art is life',
+    profilePhoto: 'https://randomuser.me/api/portraits/men/8.jpg'
+    },
+]
+
+export default photolist
